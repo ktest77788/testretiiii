a98fbbc94bef6d93ce791dda31d53f0c5ad1e25f
kamal <kamal@DESKTOP-K6ON2SM.fios-router.home>
2021-09-28 19:51:35 -0400

    My first commit

---
+++ src/main/java/pcak1/NewTest12.java
@@ -0,0 +1,22 @@
+package pcak1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest12 {
+  @Test
+  public void f() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kamal\\Desktop\\drivers246\\chromedriver.exe");
+		//opens chrome browser
+       WebDriver driver = new ChromeDriver();
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://ui.freecrm.com/");
+        
+       String atitle =  driver.getTitle();
+       String etitle = "Cogmento CRM";
+  }
+}
